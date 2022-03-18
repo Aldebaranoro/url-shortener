470b26ba9737fdc65759c06314256d53fdc08d57
Aldebaranoro <anatol.karas@gmail.com>
2022-03-18 22:09:25 +0500

    Initial commit

---
+++ src/test/java/com/github/aldebaranoro/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aldebaranoro.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

4af3344d1d9d16295d87bb56ef06e4d2c66e5586
Anatoly Karas <anatol.karas@gmail.com>
2022-03-31 22:05:45 +0500

    Deploy to Heroku
    * Add GitHub CI
    
    * Add Procfile for deploying in Heroku
    
    * Add liquibase migrations
    
    * Update dependencies
    
    * Fix test contextLoads
    
    * Add system.properties for Heroku
    
    Closes #1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Xmx256m -jar build/libs/url-shortener-0.0.1-SNAPSHOT.jar --spring.profiles.active=default,prod --server.port=$PORT $JAVA_OPTS
---
+++ src/test/java/com/github/aldebaranoro/urlshortener/test/Constraints.java
@@ -0,0 +1,6 @@
+package com.github.aldebaranoro.urlshortener.test;
+
+public class Constraints {
+
+    public static final String POSTGRES_IMAGE = "postgres:14.2-alpine3.15";
+}
